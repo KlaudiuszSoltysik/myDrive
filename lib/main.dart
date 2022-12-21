d350e6e9a5b9bec58fc1448970cc374eb61234d4
Klaudiusz Sołtysik <klaudiusz.s1405@gmail.com>
2022-12-21 20:29:04 +0100

    21.12 work

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,32 +11,62 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
-        title: 'myDrive',
-        theme: ThemeData(
-          brightness: Brightness.light,
-          primaryColor: Colors.blue[700],
-          fontFamily: 'Montserrat',
-        ),
-        home: Scaffold(
-          body: Center(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                FractionallySizedBox(
-                  widthFactor: 0.9,
-                  child: FittedBox(
-                    fit: BoxFit.fitWidth,
-                    child: Text(
-                      'myDrive',
-                      style: GoogleFonts.racingSansOne(color: Colors.blue[700]),
+      title: 'myDrive',
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primaryColor: Colors.blue[700],
+        fontFamily: 'Montserrat',
+      ),
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  Image.asset('assets/images/car.png'),
+                  FractionallySizedBox(
+                    widthFactor: 0.9,
+                    child: FittedBox(
+                      fit: BoxFit.fitWidth,
+                      child: Text(
+                        'myDrive',
+                        style:
+                            GoogleFonts.racingSansOne(color: Colors.blue[700]),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: 15, horizontal: 25),
+                  child: Text(
+                    'Sign in with Google',
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.w600),
+                  ),
+                ),
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.blue[700]!),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
                     ),
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
